3907cea8bf851c32dead0b518ad3aea8cb2bc354
Daniel Husar <dano.husar@gmail.com>
2014-11-17 16:51:25 +0100

    home newly filters

--- home.js
+++ home.js
@@ -1,70 +1,92 @@
-(function(window, document, $, undefined){
+(function(window, document, $J, undefined){
   'use strict';
 
-  window.UpdateFrontPage = function(){
-    if ( g_bMarketWindowHidden )
+  $J('body').append('<div id="hold" style="display:none;"></div>');
+  var whitelistRegexp = /knife|karambit|bayonet|awp|m4a4|m4a1|ak-47|glock-18/gi;
+  var cache = {};
+
+  window.LoadRecentListings = function ( id, type, rows ) {
+    if ( g_bBusyLoadingMore )
     {
-      setTimeout( UpdateFrontPage, g_nMillisPopularRefresh );
       return;
     }
 
-    $J.ajax( {
-      url: 'http://steamcommunity.com/market/popular',
-      type: 'GET',
-      data: {
+    var elShowMore = $(id);
+    var elRows = $(rows);
+
+    $J('#sellListingsMore').css('color', '#171717');
+
+    g_bBusyLoadingMore = true;
+    new Ajax.Request( 'http://steamcommunity.com/market/recent', {
+      method: 'get',
+      parameters: {
         country: g_strCountryCode,
-        appid: 730,
         language: g_strLanguage,
-        currency: typeof( g_rgWalletInfo ) != 'undefined' ? g_rgWalletInfo['wallet_currency'] : 1,
-        count: 10
-      }
-    } ).error( function ( ) {
-      setTimeout( UpdateFrontPage, g_nMillisPopularRefresh );
-    } ).success( function( data ) {
-      if ( data.stop )
-      {
-        return;
-      }
+        currency: typeof( g_rgWalletInfo ) != 'undefined' ? g_rgWalletInfo['wallet_currency'] : 1
+      },
+      onSuccess: function( transport ) {
+        if ( transport.responseJSON )
+        {
+          var response = transport.responseJSON;
 
-      if ( data.success )
-      {
-        var nMilliToWaitForRowUpdate = 0;
+          if ( response.assets.length != 0 )
+          {
+            g_rgRecents[type]['time'] = response.last_time;
+            g_rgRecents[type]['listing'] = response.last_listing;
 
-        data.results_html = data.results_html.filter(function(item, i){
-          var ret = item.match('<span class="market_listing_game_name">Counter-Strike: Global Offensive</span>') ? true : false;
-          if (ret) {
-            data.data.splice(i, 1);
+
+            $J('#hold').html(response.results_html);
+            $J('#hold .market_listing_row').each(function () {
+              var text = $J(this).html();
+              var name = $J(this).find('.market_listing_item_name_link').text().trim();
+              var price = $J(this).find('.market_listing_price').text().trim();
+              var ident = (name + price).replace(/(\r\n|\n|\r)/gm,'').replace(/\s/g, '-');
+
+              if (!text) {
+                $J(this).remove();
+              } else {
+                var csgo = text.match('<span class="market_listing_game_name">Counter-Strike: Global Offensive</span>');
+                var whitelist = name.match(whitelistRegexp);
+
+                if ((!csgo || !whitelist) || cache[ident]) {
+                  $J(this).remove();
+                }
+
+                cache[ident] = true;
+              }
+            });
+            response.results_html = $J('#hold').html();
+
+            elRows.insert( { 'bottom' : response.results_html } );
+            MergeWithAssetArray( response.assets );
+            MergeWithListingInfoArray( response.listinginfo );
+            MergeWithAppDataArray( response.app_data );
+            eval( response.hovers );
+
+            $J('#sellListingsMore').css('color', '#939393');
           }
-          return ret;
-        });
-
-        console.log(data);
-
-        // Build a list of rows to be updated
-        var rgElems = [];
-        for ( var i = 0; i < data.results_html.length; i++ )
-        {
-          rgElems.push( i );
         }
+      },
+      onComplete: function() {
+        g_bBusyLoadingMore = false;
 
-        // Update those rows randomly
-        rgElems = v_shuffle( rgElems );
-
-        for ( var i = 0; i < rgElems.length; i++ )
-        {
-          setTimeout( CreatePopularItemClosure(data, rgElems[i]), nMilliToWaitForRowUpdate );
-          nMilliToWaitForRowUpdate += ( g_nMillisPopularRefresh / data.results_html.length );
-        }
-
-        setTimeout(
-          function() {
-            g_rgPreviousPopularData = data.data;
-            UpdateFrontPage();
-          }, nMilliToWaitForRowUpdate
-        );
       }
-    } );
-  }
+    });
+  };
+
+  $J('.market_tab_well_tab ').removeClass('market_tab_well_tab_active');
+  $J('.market_tab_well_tab ').addClass('market_tab_well_tab_inactive');
+  $J('#tabRecentSellListings').removeClass('market_tab_well_tab_inactive').addClass('market_tab_well_tab_active');
+  $J('.market_content_block').hide();
+  $J('#sellListingsTable').show();
+
+  LoadRecentListings( 'sellListingsMore', 'sell_new', 'sellListingRows' );
+  setTimeout(function () {
+    LoadRecentListings( 'sellListingsMore', 'sell_new', 'sellListingRows' );
+  }, 200);
+  setTimeout(function () {
+    LoadRecentListings( 'sellListingsMore', 'sell_new', 'sellListingRows' );
+  }, 300);
 
 
 })(this, this.document, this.$J);
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Steam market chrome extension
+
+> Steam market modification for counter strike global offensive.
+
+## Installation
+
+Load it as unpacked extension. (I'm not going to submit it to chrome store)
+
+## What it does
+
+On http://steamcommunity.com/market it will by default show newly listed items, filtered only by cs:go, and only some kinds of weapons (see home.js to modify it)
+
+On http://steamcommunity.com/market/search you can search multiple items, if you separate them by comma
+
+
+## License
+
+MIT © Daniel Husar
